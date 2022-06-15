241e78b451c08d5d8c4490aa7e19a0872c4f5f8c
teodorstrom <teodorstrom.ts@gmail.com>
2022-06-15 18:59:30 +0000

    Users can sign in, sign up, leave a comment with approval from Admin

--- blog/admin.py
+++ blog/admin.py
@@ -17,7 +17,6 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
